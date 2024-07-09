5f70f16f7a0dfb887e3d03b954677db1303b009d
brachiGH <40743558+brachiGH@users.noreply.github.com>
2024-07-10 00:49:26 +0100

    fixes forgit to commit

--- Components/Generating_ControlUnit_Logic.py
+++ Components/Generating_ControlUnit_Logic.py
@@ -7,6 +7,8 @@ for i in range(2**5):
 
 Fetch = ["PO", "RO", "II"]
 Increment_PC = ["INC"]
+
+# note the logic for BRN, BRZ and BRV is implemented in the Control unit
 Instructions = {"ALUoperations":["ACO", "AI"],"BIN":["IOD", "BI"],"CIN":["IOD", "CI"],"LDA":["IOA", "RO", "AI"],"LDB":["IOA", "RO", "BI"],"LDC":["IOA", "RO", "CI"],"STA":["IOA", "AO", "RI"], "BR":["IOA", "PI"],"LR":["IOA", "RO", "LI"],"LR2":["LO", "RO", "AI"],"NOP":[]}
 Opcodes = {'0000': 'NOP', '1100': 'LDA', '0001': 'LDB', '0010': 'LDC', '0011': 'BIN', '0100': 'CIN', '0101': 'STA', '0110': 'BR', '1010': 'LR', '1011': 'LR2', '1110': 'ALUoperations'}
 MicroInstructionBitCoefficient = {"AO":0, "BO":1, "CO":2, "ACO":3,"IOD":4, "PO":5, "LO":6, "IOA":7, "AI":8, "BI":9, "CI":10, "II":11, "PI":12, "LI":13, "PID":14, "INC":15, "RESET":16, "CinALU":17, "RI":18, "RO":19}
