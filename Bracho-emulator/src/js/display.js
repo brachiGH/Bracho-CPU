d660dd4dbf7119db254006a726f8fa3e27be6ec5
brachiGH <40743558+brachiGH@users.noreply.github.com>
2024-07-11 23:15:46 +0100

    init

--- Bracho-emulator/src/js/display.js
+++
@@ -1,9 +0,0 @@
-// const display = ref(Array(64).fill(Array(64).fill(0)))
-
-export function praseDisplayOUT(input) {
-    let color = input & 0b1111;
-    let y = (input & 0b1111110000) >> 4;
-    let x = (input & 0b1111110000000000) >> 10;
-
-    return {"color": color, "x": x, "y": y}
-}
